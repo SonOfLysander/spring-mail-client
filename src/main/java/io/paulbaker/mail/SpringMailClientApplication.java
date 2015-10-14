16bb4e02aef6600e39a7a702c96c14352c0b4213
Paul Baker <paul.baker@overstock.com>
2015-10-14 14:44:11 -0600

    formatting

--- src/main/java/io/paulbaker/mail/SpringMailClientApplication.java
+++ src/main/java/io/paulbaker/mail/SpringMailClientApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringMailClientApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(SpringMailClientApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(SpringMailClientApplication.class, args);
+  }
 }
