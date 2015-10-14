1d00b524b8fbf3730dc981fb83c1290b4beb8fcb
Paul Baker <paul.baker@overstock.com>
2015-10-14 11:01:16 -0600

    Initial commit.

---
+++ src/main/java/io/paulbaker/mail/SpringMailClientApplication.java
@@ -0,0 +1,12 @@
+package io.paulbaker.mail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMailClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMailClientApplication.class, args);
+    }
+}
